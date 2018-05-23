f8e2d4ad5d991922ed73cc7a3ef36c0103cef33f
dburns1 <patriciadburns@gmail.com>
2018-05-23 12:56:07 -0700

    function updates to check win

--- main.js
+++ main.js
@@ -1,33 +1,35 @@
 
 
-
-var gameBoardArray = 
-[[0, 0, 0, 1, 1, 2, 1],
-[0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0]];
+var gameBoardArray =
+    [[0, 0, 0, 1, 2, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]];
 
 var vectors = [
-{ y: 0, x: -1}, //left
-{y: 0, x: 1 }, //right 
-{y: 1, x: 0 }, // Not handling up because of no piece above
-{ y: -1, x: -1},{y: 1, x: 1 }, //up left and down right 2// 
-{ y: -1, x: 1}, {y: 1, x: -1 }, //up right and down left 3// 
- ];
+    { y: 0, x: -1, axis: "HorizontalAxis" }, //going left and not y 
+    { y: 0, x: 1, axis: "HorizontalAxis" }, //going right and not y 
+    { y: 1, x: 0, axis: "verticalAxis" }, //going down and not x 
+    { y: -1, x: -1, axis: "verticalAxis" }, // going up and and left 
+    { y: 1, x: 1, axis: "diagonalRight" }, //going down and going right 
+    { y: -1, x: 1, axis: "diagonalRight" }, // up and right 
+    { y: 1, x: -1, axis: "diagonalLeft" }, //down and left 
+    { y: -1, x: 1, axis: "diagonalLeft" }, //down and left 
+];
 
 
-function checkingForAWin(){
+function checkForWin(y, x, symbol) {
+    debugger;
+    var counter = 1;
     for (var majorDirection in vectors) {
-        set = vectors[majorDirection];
-        var counter = 1;
-    
-        var startY = 0 + set['y'];
-        var startX = 3 + set['x'];
-    
-        while(gameBoardArray[startY][startX] !== undefined) {
+        var set = vectors[majorDirection];
+        var startY = y + set['y'];
+        var startX = x + set['x'];
+
+        while (gameBoardArray[startY][startX] === symbol) {
             console.log(gameBoardArray[startY][startX]);
             //check to see if player1/ player2 or 0 
             //player1 was defined then an if statement to proceed 
@@ -35,11 +37,23 @@ function checkingForAWin(){
             counter++;
             startY += set['y'];
             startX += set['x'];
+            if (counter === 4) {
+                console.log("YOU WIN!");
+                return true;
+            }
+        }
+        if (gameBoardArray[startY][startX] != symbol) {
+            counter = 1;
+            continue;
         }
     }
-    
 }
 
+checkForWin(0, 3, 1);
 
-
+// if(checkForWin(0,3,1) === true) {
+//     //display modal
+// } else {
+//     // click function enabled continue playing game 
+// }
 
