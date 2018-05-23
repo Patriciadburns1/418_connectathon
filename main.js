3031553a783c6f1a0af9c11a4434ec0fe1c122c7
jhonalino <jhonfrancisalino@gmail.com>
2018-05-23 11:16:27 -0700

    fixed missing closing braces from previous commit

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 $(document).ready(startConnectFour);
 
 function startConnectFour() {
-    console.log("start Connect Four");
+  console.log("start Connect Four");
   createCells(7, 7);
-    addGameHandlers();
+  addGameHandlers();
 }
 
 function createCells(row, col) {
@@ -23,34 +23,35 @@ function createCells(row, col) {
         class: "hole"
       });
 
-    //   var img = $('<img src="images/earth_icon.png" alt="">');
-    //   img.appendTo(hole);
-      
+      //   var img = $('<img src="images/earth_icon.png" alt="">');
+      //   img.appendTo(hole);
+
       hole.appendTo(cellContainer);
 
       gameBoard.append(cellContainer);
     }
   }
+}
 
 function addGameHandlers() {
-    $(".game-board").on("click", ".cell-container", updateArrayAtPosition);
+  $(".game-board").on("click", ".cell-container", updateArrayAtPosition);
 }
 
 var coordinateColumn = null;
 var currentSymbol = null;
 
-function updateArrayAtPosition(coordinateColumn, currentSymbol){
-    if (multiArray[2][coordinateColumn] === 0) {
-       multiArray[2].splice(coordinateColumn, 1, currentSymbol)
-       console.log("Filled bottom row in column");
-      } else if (multiArray[1][coordinateColumn] === 0) {
-        multiArray[1].splice(coordinateColumn, 1, currentSymbol) 
-        console.log("Filled middle row in column");
-      } else if (multiArray[0][coordinateColumn] === 0) {
-        multiArray[0].splice(coordinateColumn, 1, currentSymbol)
-        console.log("Filled top row in column");
-      } else {
-        console.log("Column is filled");
-      }
-    return multiArray;
+function updateArrayAtPosition(coordinateColumn, currentSymbol) {
+  if (multiArray[2][coordinateColumn] === 0) {
+    multiArray[2].splice(coordinateColumn, 1, currentSymbol);
+    console.log("Filled bottom row in column");
+  } else if (multiArray[1][coordinateColumn] === 0) {
+    multiArray[1].splice(coordinateColumn, 1, currentSymbol);
+    console.log("Filled middle row in column");
+  } else if (multiArray[0][coordinateColumn] === 0) {
+    multiArray[0].splice(coordinateColumn, 1, currentSymbol);
+    console.log("Filled top row in column");
+  } else {
+    console.log("Column is filled");
+  }
+  return multiArray;
 }
