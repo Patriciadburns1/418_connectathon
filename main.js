b7fa74dabac35fbe098a39b3d47f6bdf36700ca7
vokamura <vcokamura@gmail.com>
2018-05-23 11:35:06 -0700

    Implemented getCellIndex and getCurrentSymbol and handleColumnClick functions

--- main.js
+++ main.js
@@ -6,24 +6,41 @@ function startConnectFour() {
 }
 
 function addGameHandlers() {
-    $(".game-board").on("click", ".cell-container", updateArrayAtPosition);
+    $(".game-board").on("click", ".cell-container", handleColumnClick);
 }
 
 var coordinateColumn = null;
 var currentSymbol = null;
 
+function handleColumnClick() {
+    getCellIndex();
+    getCurrentSymbol();
+    updateArrayAtPosition();
+}
+
+function getCellIndex(){
+    // coordinateRow = $(this).find(".cell-container").attr("row");
+    coordinateColumn = $(this).find(".cell-container").attr("column");
+    return coordinateColumn;
+}
+
+function getCurrentSymbol() {
+    currentSymbol = $(this).find(".cell-container").attr("src");
+    return currentSymbol;
+}
+
 function updateArrayAtPosition(coordinateColumn, currentSymbol){
-    if (multiArray[2][coordinateColumn] === 0) {
-       multiArray[2].splice(coordinateColumn, 1, currentSymbol)
+    if (gameBoardArray[2][coordinateColumn] === 0) {
+       gameBoardArray[2].splice(coordinateColumn, 1, currentSymbol)
        console.log("Filled bottom row in column");
-      } else if (multiArray[1][coordinateColumn] === 0) {
+      } else if (gameBoardArray[1][coordinateColumn] === 0) {
         multiArray[1].splice(coordinateColumn, 1, currentSymbol) 
         console.log("Filled middle row in column");
-      } else if (multiArray[0][coordinateColumn] === 0) {
-        multiArray[0].splice(coordinateColumn, 1, currentSymbol)
+      } else if (gameBoardArray[0][coordinateColumn] === 0) {
+        gameBoardArray[0].splice(coordinateColumn, 1, currentSymbol)
         console.log("Filled top row in column");
       } else {
         console.log("Column is filled");
       }
-    return multiArray;
+    return gameBoardArray;
 }
