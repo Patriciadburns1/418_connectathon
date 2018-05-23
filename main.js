ecfe4a6115754beb68770b3dfebf1ea83d0a17c7
jhonalino <jhonfrancisalino@gmail.com>
2018-05-23 16:24:54 -0700

    updated checkForWin with another loop

--- main.js
+++ main.js
@@ -1,27 +1,35 @@
-
-
 $(document).ready(startConnectFour);
 
-var gameBoardArray =
-    [[0, 0, 0, 1, 2, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0]];
-
-var vectors = [
-    { y: 0, x: -1, axis: "HorizontalAxis" }, //going left and not y 
-    { y: 0, x: 1, axis: "HorizontalAxis" }, //going right and not y 
-    { y: 1, x: 0, axis: "verticalAxis" }, //going down and not x 
-    { y: -1, x: -1, axis: "verticalAxis" }, // going up and and left 
-    { y: 1, x: 1, axis: "diagonalRight" }, //going down and going right 
-    { y: -1, x: 1, axis: "diagonalRight" }, // up and right 
-    { y: 1, x: -1, axis: "diagonalLeft" }, //down and left 
-    { y: -1, x: 1, axis: "diagonalLeft" }, //down and left 
+var gameBoardArray = [
+  [1, 1, 1, 1, 2, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0]
 ];
 
+var vectors = [
+  { y1: 0, x1: -1, y2: 0, x2: 1 }, //left and right
+  { y1: 1, x1: 0, y2: -1, x2: 0 }, //up and down
+  { y1: 1, x1: 1, y2: -1, x2: -1 }, //down right and up left
+  { y1: 1, x1: -1, y2: -1, x2: 1 } //down left and up right
+];
+
+var player = 1;
+
+function togglePlayer(player) {
+    player = 1 - player;
+    return player;
+}
+
+var symbols = [
+    "images/earth_icon.jpg",
+    "images/mars_icon.jpg",
+]
+
+checkForWin(0, 3, 1);
 
 function startConnectFour() {
   console.log("start Connect Four");
@@ -56,82 +64,92 @@ function createCells(row, col) {
 }
 
 function addGameHandlers() {
-    $(".game-board").on("click", ".cell-container", handleColumnClick);
+  $(".game-board").on("click", ".cell-container", handleColumnClick);
 }
 
 var coordinateColumn = null;
 var currentSymbol = null;
 
 function handleColumnClick() {
-    getCellIndex();
-    getCurrentSymbol();
-    updateArrayAtPosition();
+  getCellIndex();
+  getCurrentSymbol();
+  updateArrayAtPosition();
 }
 
-function getCellIndex(){
-    // coordinateRow = $(this).find(".cell-container").attr("row");
-    coordinateColumn = $(this).find(".cell-container").attr("column");
-    return coordinateColumn;
+function getCellIndex() {
+  // coordinateRow = $(this).find(".cell-container").attr("row");
+  coordinateColumn = $(this)
+    .find(".cell-container")
+    .attr("column");
+  return coordinateColumn;
 }
 
 function getCurrentSymbol() {
-    currentSymbol = $(this).find(".cell-container").attr("src");
-    return currentSymbol;
+  currentSymbol = $(this)
+    .find(".cell-container")
+    .attr("src");
+  return currentSymbol;
 }
 
-function updateArrayAtPosition(coordinateColumn, currentSymbol){
-    if (gameBoardArray[2][coordinateColumn] === 0) {
-       gameBoardArray[2].splice(coordinateColumn, 1, currentSymbol)
-       console.log("Filled bottom row in column");
-      } else if (gameBoardArray[1][coordinateColumn] === 0) {
-        multiArray[1].splice(coordinateColumn, 1, currentSymbol) 
-        console.log("Filled middle row in column");
-      } else if (gameBoardArray[0][coordinateColumn] === 0) {
-        gameBoardArray[0].splice(coordinateColumn, 1, currentSymbol)
-        console.log("Filled top row in column");
-      } else {
-        console.log("Column is filled");
-      }
-    return gameBoardArray;
+function updateArrayAtPosition(coordinateColumn, currentSymbol) {
+  if (gameBoardArray[2][coordinateColumn] === 0) {
+    gameBoardArray[2].splice(coordinateColumn, 1, currentSymbol);
+    console.log("Filled bottom row in column");
+  } else if (gameBoardArray[1][coordinateColumn] === 0) {
+    multiArray[1].splice(coordinateColumn, 1, currentSymbol);
+    console.log("Filled middle row in column");
+  } else if (gameBoardArray[0][coordinateColumn] === 0) {
+    gameBoardArray[0].splice(coordinateColumn, 1, currentSymbol);
+    console.log("Filled top row in column");
+  } else {
+    console.log("Column is filled");
+  }
+  return gameBoardArray;
 }
-
-
-
 
 function checkForWin(y, x, symbol) {
-    debugger;
+  for (var majorDirection in vectors) {
     var counter = 1;
-    for (var majorDirection in vectors) {
-        var set = vectors[majorDirection];
-        var startY = y + set['y'];
-        var startX = x + set['x'];
+    var set = vectors[majorDirection];
+    var startY1 = y + set["y1"];
+    var startX1 = x + set["x1"];
+    var startY2 = y + set["y2"];
+    var startX2 = x + set["x2"];
 
-        while (gameBoardArray[startY][startX] === symbol) {
-            console.log(gameBoardArray[startY][startX]);
-            //check to see if player1/ player2 or 0 
-            //player1 was defined then an if statement to proceed 
-            //only increment counter if it matches current player piece 
-            // we need to check if on same axis, if not on same axis and the symbol isnt the same
-            //we switch to a new axis 
-            counter++;
-            startY += set['y'];
-            startX += set['x'];
-            if (counter === 4) {
-                console.log("YOU WIN!");
-                return true;
-            }
-        }
-        if (gameBoardArray[startY][startX] != symbol) {
-            counter = 1;
-            continue;
-        }
+    //check to see if player1/ player2 or 0
+    //player1 was defined then an if statement to proceed
+    //only increment counter if it matches current player piece
+    // we need to check if on same axis, if not on same axis and the symbol isnt the same
+    //we switch to a new axis
+    while (
+      gameBoardArray[startY1] &&
+      gameBoardArray[startY1][startX1] === symbol
+    ) {
+      counter++;
+      startY1 += set["y1"];
+      startX1 += set["x1"];
     }
-}
 
-checkForWin(0, 3, 1);
+    while (
+      gameBoardArray[startY2] &&
+      gameBoardArray[startY2][startX2] === symbol
+    ) {
+      counter++;
+      startY2 += set["y2"];
+      startX2 += set["x2"];
+    }
+
+    if (counter >= 4) {
+      console.log("YOU WIN!");
+      return true;
+    }
+  }
+  console.log("no match!");
+  return false;
+}
 
 // if(checkForWin(0,3,1) === true) {
 //     //display modal
 // } else {
-//     // click function enabled continue playing game 
+//     // click function enabled continue playing game
 // }
